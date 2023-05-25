800e87c0639436faf442821ea0f7749bf3169aa9
Oli Guei <olivrg@gmail.com>
2023-05-25 09:12:44 +0100

    Build search spider

---
+++ search_spider.py
@@ -0,0 +1,41 @@
+
+import json
+import re
+from urllib.parse import urlencode
+
+import scrapy
+
+
+class IndeedJobSpider(scrapy.Spider):
+    name = "indeed_jobs"
+
+    def get_indeed_search_url(self, keyword, location, offset=0):
+        parameters = {"q": keyword, "l": location, "filter": 0, "start": offset}
+        return "https://www.indeed.com/jobs?" + urlencode(parameters)
+
+    def start_requests(self):
+        keyword_list = ['python']
+        location_list = ['texas']
+        for keyword in keyword_list:
+            for location in location_list:
+                indeed_jobs_url = self.get_indeed_search_url(keyword, location)
+                yield scrapy.Request(url=indeed_jobs_url, callback=self.parse_search_results, meta={'keyword': keyword, 'location': location, 'offset': 0})
+
+    def parse_search_results(self, response):
+        location = response.meta['location']
+        keyword = response.meta['keyword']
+        offset = response.meta['offset']
+        script_tag = re.findall(r'window.mosaic.providerData\["mosaic-provider-jobcards"\]=(\{.+?\});', response.text)
+        if script_tag is not None:
+            json_blob = json.loads(script_tag[0])
+
+            # Paginate Through Jobs Pages
+            if offset == 0:
+                meta_data = json_blob["metaData"]["mosaicProviderJobCardsModel"]["tierSummaries"]
+                num_results = sum(category["jobCount"] for category in meta_data)
+                if num_results > 1000:
+                    num_results = 50
+
+                for offset in range(10, num_results + 10, 10):
+                    url = self.get_indeed_search_url(keyword, location, offset)
+                    yield scrapy.Request(url=url, callback=self.parse_search_results, meta={'keyword': keyword, 'location': location, 'offset': offset})
